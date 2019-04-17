79f71502477d1e5e0ca222cd2e7ff069f9c31125
Peter <clerkin07@gmail.com>
2019-04-17 16:51:48 +0100

    Updates

---
+++ precache-manifest.b265a765363d4fae4b40b7d03a2f47a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58b1e55fe5573bceb9cd",
+    "url": "/neighbourhood-map/static/css/main.2b399b61.chunk.css"
+  },
+  {
+    "revision": "58b1e55fe5573bceb9cd",
+    "url": "/neighbourhood-map/static/js/main.c1bbcc97.chunk.js"
+  },
+  {
+    "revision": "d103543d2b03b8c7c24b",
+    "url": "/neighbourhood-map/static/js/runtime~main.eb5548ae.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/neighbourhood-map/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neighbourhood-map/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "91a9bd54c629a0f8c46d64a3910036f6",
+    "url": "/neighbourhood-map/index.html"
+  }
+];
